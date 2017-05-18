70754a84b87a841ad01dac53e6db3b1152af2432
joaquinsanchiz <alu0100893755@ull.edu.es>
2017-05-18 22:48:37 +0100

    Update documentation

--- src/twitterBot/UsersToFollow.java
+++
@@ -1,9 +0,0 @@
-package twitterBot;
-
-public class UsersToFollow extends UsersToSave{
-	
-	public UsersToFollow(){
-		super();
-	}
-
-}
--- src/twitterBot/UsersToMail.java
+++
@@ -1,9 +0,0 @@
-package twitterBot;
-
-public class UsersToMail{
-	
-	public static void insertNewUser(String mail, String userName){
-		FileWriting.writeMail(mail, userName);
-	}
-
-}
